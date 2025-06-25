eed76385109fef5bce1e90986f13774ef57f56fe
jose <pucciarellijose@gmail.com>
2025-06-25 18:47:05 -0300

    making layout for sign in and connecting to appwrite server

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Store - The storage you may need</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Store - The storage you may need</h1>
     </div>
   );
 }
